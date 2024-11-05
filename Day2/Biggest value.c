870cfa3ac522e1c1e31b83df5e374131756dac5c
Prudhvi-69 <prudhvi.touch.me@gmail.com>
2024-11-05 17:54:59 +0530

    updated

---
+++ Day2/Biggest value.c
@@ -0,0 +1,138 @@
+#include<stdio.h>
+void main()
+{
+	char g=getc(stdin);
+	
+	if(g=='A')//once you are done writing
+		printf("Good Student");
+	else if(g=='B')//let me know
+		printf("Little Good STudent");
+	else if(g=='C')//i will let you use the systems
+		printf("Good Student again, but...");
+	else if(g=='D')//for some time
+		printf("Ushhhhh, goood student.....");
+	else if(g=='E')//for solving few VERY EASY questions
+		printf("go and pay exam fees");
+	else 
+		printf("Idiotttt, go, write the exam");
+	
+	printf("\n value of G : %d ",g);
+	
+	
+	
+	
+//	int v1,v2,v3;//240,106,690
+//	scanf("%d%d%d",&v1,&v2,&v3);
+//	if(v1>v2)
+//	{
+//		if(v1>v3)
+//			printf("biggest value is v1");
+//		else
+//			printf("biggest value is v3");
+//	}
+//	else
+//	{
+//		if(v2>v3)
+//			printf("biggest value is v2");
+//		else
+//			printf("biggest value is v3");
+//	}
+//	
+//( (v1>v2)?( (v1>v3)?printf("v1"):printf("v3")):
+// 	((v2>v3)?printf("v2"):printf("v3")));
+//	     //true 	
+//	     
+//	     printf("hello \
+////this is multi line print statement");
+//int a=1,b=4;
+//if(a>b)
+//	printf("helloo");
+//else
+//	printf("thanks");
+//
+
+
+
+}
+
+
+
+
+/*
+int a=1,b=4;
+if(a>b)
+	printf("helloo");
+printf("thanks");
+
+
+char g;
+scanf
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
