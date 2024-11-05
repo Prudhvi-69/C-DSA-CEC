f665930e989ff9f65c2955c65cfe9576599a2892
Prudhvi-69 <prudhvi.touch.me@gmail.com>
2024-11-05 19:02:27 +0530

    Day1 dump

---
+++ Day1/Day1 AN.c
@@ -0,0 +1,140 @@
+/*
+
+Arithmetic Ops :
+Arithmetic Binary Ops : +,-,*,/,%
+Arithmetic Unary ops : ++, --
+
+& -> Address of
+* -> value at
+
+.  -> access the members of structure
+-> : accessing the members of structure with pointer
+
+
+
+infix -> 6 * 9
+prefix ->  * 6 9
+postfix -> 6 9 *
+
+
+
+
+short int -> 2 bytes
+int -> 18 19 -> 4 bytes
+long int -> 8 bytes
+float -> 19 19.5 20 -> 4 bytes
+9.824577
+double -> 3.14159265 -> 8 bytes
+
+
+character -> -128 to 127 (0)
+1 byte
+
+
+
+
+
+
+
+
+*/
+#include<stdio.h>
+void main()
+{
+//Arithmetic OPerators and their operations
+/*
+//relational operators :
+<
+>
+==
+<=
+>=
+!=
+
+
+*/
+
+
+
+
+	integral data
+		->int
+		->float
+		->double
+	textual data
+		->char
+		->string
+	Boolean data
+		-> true    -> 1
+		-> false   -> 0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+
+	int value1=12,value2=25;
+	int result;
+	result = value1 + value2;
+	//result = 0-> 267
+	//value1= 10, value2= 257
+//ASCII of `0-> \0
+//	printf(" Result : %i ",result);
+
+	char x=getc(stdin);
+	putc(x,stdout);
+
+	gets();
+	puts();
+
+scanf();
+printf();
+
+	int -> %d,%i,%u,
+	long -> %l
+	float -> %f
+	char -> %c
+	string -> %s
+	long int -> %ld
+	long float -> %lf
+	long int dd=25268698276;
+	int -> 474521541
+
+//Write a program by working with
+//Arithmatic ops....\
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+int a=10, b=20;
+
+printf("%d",++a + b++);
+
+
+
+
+
+
+
+
---
+++ Day1/day1 samplpe.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdbool.h>
+void main()
+{
+//	int data1,data2,add,sub,mul,div,mod;
+//     data1=55;
+//	data2=70;	//C lan
+//	add=data1+data2;
+//	sub=data1-data2;
+//	mul=data1*data2;
+//	div=data1/data2;  -> quotient
+//	mod=data1%data2;  -> remainder
+//	printf("\nADD : %d ",add);
+//	printf("\nSUB : %d ",sub);
+//
+/* this is going to be the comment and you have r
+each the new line but it is going to be consider
+ed as the comment
+zdktjhgksedmgtp0
+
+	int data1,data2;
+	scanf("%d%d",&data1,&data2);//69 42
+	int result = data1==data2;
+	printf("Result : %d",result);
+	bool tf=false;
+	bool ft=true;
+
+    unsigned int var;
+    scanf("%o",&var);
+    printf("\n value : %d",var);
+
+*/
+
+int a=1;
+float b=2.0;
+printf("%d ",sizeof(a+b));
+
+
+
+
+
+}
