a04759a1c2d47b694def03accfca401596f7c059
Prudhvi-69 <prudhvi.touch.me@gmail.com>
2024-11-05 17:44:42 +0530

    day2 end

---
+++ Day2/try.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+void main()
+{
+//     int value;
+//     scanf("%d",&value);
+//     if(value>=10)
+//         if(value<=20)
+//             printf("CITYYYYYYYYYYYYYYYYYY");
+//     else  
+//     {    if(value==100)
+//            printf("badCITYYYYYYYYYYYYYYY");
+//     }
+// // }
+//     char x;
+//     scanf("%c",&x);//A
+//     printf("OUTPUT : %c",x-1);//65-1=64=@
+
+//     int value=150;
+// //    scanf("%d",&value);
+//     if(value>=10 || value<=20)
+//         printf("Hiiiiii, Good Studentssssss");
+//     else
+//         printf("Baddd Studentssss you are");
+    // char x=getc(stdin);
+
+    // if(x>='A' && x<=90)//x>=65 && x<=90 | x>='A' && x<='Z'
+    //     x=x+32;
+    // else if(x>='a' && x<='z')
+    //     x=x-32;
+    
+    // printf("VALUE IS : %c",x);
+
+    int v1;
+    scanf("%d",&v1);
+    float res=v1/2.0;  // it is known as floating point division
+    printf("\nResult : %f5",res);//to get the accurate results 
+//when the division is performed
+}
